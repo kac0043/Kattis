d44c2943accd3028e694ed9a6f3bf914837a2efd
Kaitlyn Childers <kac0043@auburn.edu>
2016-02-29 22:28:33 -0600

    Aaah!
    Kattis Problem:
    Author: Christian Neverdal Jonassen. Source: Nordic Collegiate
    Programming Contest 2012.

---
+++ aah.java
@@ -0,0 +1,75 @@
+import java.util.*; 
+import java.io.*;
+
+public class aah
+{
+	public static void main(String[] args)
+	{
+		Kattio scan = new Kattio(System.in, System.out);
+		String a = scan.getWord(); 
+		String b = scan.getWord();
+
+		if (a.length() < b.length())
+			System.out.println("no");
+		else 
+			System.out.println("go");
+	}
+
+
+	public static class Kattio extends PrintWriter {
+    public Kattio(InputStream i) {
+	super(new BufferedOutputStream(System.out));
+	r = new BufferedReader(new InputStreamReader(i));
+    }
+    public Kattio(InputStream i, OutputStream o) {
+	super(new BufferedOutputStream(o));
+	r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public boolean hasMoreTokens() {
+	return peekToken() != null;
+    }
+
+    public int getInt() {
+	return Integer.parseInt(nextToken());
+    }
+
+    public double getDouble() { 
+	return Double.parseDouble(nextToken());
+    }
+
+    public long getLong() {
+	return Long.parseLong(nextToken());
+    }
+
+    public String getWord() {
+	return nextToken();
+    }
+
+
+
+    private BufferedReader r;
+    private String line;
+    private StringTokenizer st;
+    private String token;
+
+    private String peekToken() {
+	if (token == null) 
+	    try {
+		while (st == null || !st.hasMoreTokens()) {
+		    line = r.readLine();
+		    if (line == null) return null;
+		    st = new StringTokenizer(line);
+		}
+		token = st.nextToken();
+	    } catch (IOException e) { }
+	return token;
+    }
+
+    private String nextToken() {
+	String ans = peekToken();
+	token = null;
+	return ans;
+    }
+}
+}
